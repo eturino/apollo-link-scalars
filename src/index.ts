ca580a06b4baf591b577a028c6ce16e5bf7c6fec
Eduardo Turiño <eturino@eturino.com>
2020-08-19 23:16:04 +0100

    style: add prettier import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import { makeExecutableSchema } from "graphql-tools";
 import { isNone } from "./lib/is-none";
 import { ScalarApolloLink, withScalars } from "./lib/link";
 import { mapIfArray } from "./lib/map-if-array";
 import { FunctionsMap, ParsingFunctionsObject } from "./types/functions-map";
-import { makeExecutableSchema } from "graphql-tools";
 
 export {
   FunctionsMap,
