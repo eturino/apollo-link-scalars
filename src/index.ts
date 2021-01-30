666be3ebe579a3a8a75a5fa361f9dbfa0ba3c14a
Eduardo Turiño <eturino@eturino.com>
2021-01-30 16:56:13 +0100

    feat: export NullFunctions type

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { isNone } from "./lib/is-none";
 import { ScalarApolloLink, withScalars } from "./lib/link";
 import { mapIfArray } from "./lib/map-if-array";
 import { FunctionsMap, ParsingFunctionsObject } from "./types/functions-map";
+import { NullFunctions } from "./types/null-functions";
 
-export { FunctionsMap, isNone, mapIfArray, ParsingFunctionsObject, ScalarApolloLink, withScalars };
+export { FunctionsMap, isNone, mapIfArray, NullFunctions, ParsingFunctionsObject, ScalarApolloLink, withScalars };
