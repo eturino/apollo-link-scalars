fc00cfaf4efccf9b71e028996a764d98d39609cf
Alfredo Delgado <alfredodelgado@rccl.com>
2021-03-16 11:32:33 -0400

    refactor(serializer): import helpers directly - see #278

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { isNone } from "./lib/is-none";
-import { ScalarApolloLink, withScalars } from "./lib/link";
-import { mapIfArray } from "./lib/map-if-array";
-import { FunctionsMap, ParsingFunctionsObject } from "./types/functions-map";
-
-export { FunctionsMap, isNone, mapIfArray, ParsingFunctionsObject, ScalarApolloLink, withScalars };
+export { isNone } from "./lib/is-none";
+export { ScalarApolloLink, withScalars } from "./lib/link";
+export { mapIfArray } from "./lib/map-if-array";
+export { FunctionsMap, ParsingFunctionsObject } from "./types/functions-map";
